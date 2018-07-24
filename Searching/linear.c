a229f60de88d7be617cdd6091722df5657782bcf
SubashMourougayane <subashexortian@gmail.com>
2018-07-24 20:46:58 +0530

    lol

--- Searching/linear.c
+++ Searching/linear.c
@@ -2,7 +2,8 @@
 int n;
 int x;
 void leniarSearch();
-void interpolationSearch();
+void binarySearch();
+// void interpolationSearch();
 int main()
 {
     /* code */
@@ -13,19 +14,37 @@ int main()
     }
     scanf("%d",&x);
     leniarSearch(A);
+    binarySearch(A);
     // interpolationSearch(A);
-    
+
     return 0;
 }
 void leniarSearch(int A[]){
     printf("linear Search");
     for(int i=0;i<n;i++){
         if(A[i]==x){
-            printf("%d",i);
+            printf("\n%d",i);
             break;
         }
     }
 }
+void binarySearch(int A[]){
+    int temp;
+    for(int i=0;i<n-1;i++){
+        for(int j=i;j<n-i-1;j++){
+            if(A[j]>=A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d",A[i]);
+    }
+
+}
+
 // void interpolationSearch(int A[]){
  
 // }
